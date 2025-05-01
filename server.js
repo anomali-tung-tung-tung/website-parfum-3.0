fcc13a4beda38255bd016094d53f968bce953aeb
alfatch09 <alfatch09@gmail.com>
2025-05-01 12:45:14 +0700

    server update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname)); // Untuk akses file statis
+app.use(express.static(path.join(__dirname, 'public'))); // Untuk akses file statis
 
 // Koneksi MongoDB
 mongoose.connect('mongodb://localhost:27017/Maha_parfum', {
@@ -99,7 +99,7 @@ app.delete('/api/cart/clear', async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-    console.log(`Akses websitemu di: http://localhost:${PORT}/html/products.html`);
-});
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+  });
+  
